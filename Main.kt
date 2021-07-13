fdae2dceeb48c9329e65b94e886026d36bb85ad4
Samuel Rey <samuelrey010@gmail.com>
2021-07-13 16:49:28 -0700

    Create simple menu

--- Main.kt
+++ Main.kt
@@ -1,3 +1,11 @@
 fun main(args: Array<String>) {
-    println("hello world")
+    val menu = Menu(arrayListOf(MenuGroup(arrayListOf(), arrayListOf(MenuItem("salad")))))
+    
+    println(menu)
 }
+
+data class MenuItem(val name: String)
+
+data class MenuGroup(val subgroups: List<MenuGroup>, val items: List<MenuItem>)
+
+data class Menu(val groups: List<MenuGroup>)
