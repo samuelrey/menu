eb62fb58407a460581b8d8e399ced47661f2c633
Samuel <samuelrey010@gmail.com>
2021-07-15 13:47:05 -0700

    Add rudimentary tests for findItem
    These should be made into actual tests eventually.

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,15 @@
 import src.domain.*
 
 fun main(args: Array<String>) {
+    // TODO fetch menu from database
     val menu = Menu(arrayListOf(MenuGroup(arrayListOf(), arrayListOf(MenuItem("salad")))))
+
+    // TODO move into tests
+    var item = menu.findItem("salad")
+    if (item == null)
+        println("Item should not be null")
+
+    item = menu.findItem("sandwich")
+    if (item != null)
+        println("Item should be null")
 }
