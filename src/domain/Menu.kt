9f985e561bb2de045cea724667ceabbb62aad81b
Samuel <samuelrey010@gmail.com>
2021-07-15 13:54:06 -0700

    Move recursive findItem into MenuGroup
    This may make the code more difficult to read but I'm not sure at the
    moment.

--- src/domain/Menu.kt
+++ src/domain/Menu.kt
@@ -5,24 +5,7 @@ class Menu(groups: List<MenuGroup>) {
 
     fun findItem(name: String): MenuItem? {
         for (g in groups) {
-            val item = findItemRecurse(g, name)
-            if (item != null) {
-                return item
-            }
-        }
-
-        return null
-    }
-
-    private fun findItemRecurse(group: MenuGroup, name: String): MenuItem? {
-        for (i in group.items) {
-            if (name == i.name) {
-                return i
-            }
-        }
-
-        for (sg in group.subgroups) {
-            val item = findItemRecurse(sg, name)
+            val item = g.findItem(name)
             if (item != null) {
                 return item
             }
--- src/domain/MenuGroup.kt
+++ src/domain/MenuGroup.kt
@@ -1,3 +1,23 @@
 package src.domain
 
-data class MenuGroup(val subgroups: List<MenuGroup>, val items: List<MenuItem>)
+class MenuGroup(subgroups: List<MenuGroup>, items: List<MenuItem>) {
+    val subgroups = subgroups
+    val items = items
+
+    fun findItem(name: String): MenuItem? {
+        for (i in items) {
+            if (name == i.name) {
+                return i
+            }
+        }
+
+        for (sg in subgroups) {
+            val item = sg.findItem(name)
+            if (item != null) {
+                return item
+            }
+        }
+
+        return null
+    }
+}
